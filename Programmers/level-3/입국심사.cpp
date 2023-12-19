6cf809ddbbd6f9dac189d02b8d7400a3d51ee526
Jiyeon Jeong <jiyeon2781@naver.com>
2023-12-19 14:05:04 +0900

    2023.12.19-algorithm-practice
    modify algorithm code

--- Programmers/level-3/입국심사.cpp
+++ Programmers/level-3/입국심사.cpp
@@ -8,7 +8,7 @@ long long minimumTime;
 long long BinarySearch(long long first, long long last, int n, vector<int> times);
 
 long long solution(int n, vector<int> times) {
-    minimumTime = 1000000000;
+    minimumTime = 0;
     sort(times.begin(), times.end());
     long long answer = BinarySearch(1, n * (long long) times.back(), n, times);
     return answer;
@@ -17,8 +17,10 @@ long long solution(int n, vector<int> times) {
 long long BinarySearch(long long first, long long last, int n, vector<int> times){
     if (first > last) return minimumTime;
     long long mid = (first + last) / 2;
+    // mid : 심사를 받는데 걸리는 시간
     long long count = 0;
     for (long long time : times) count += (mid / time);
+    // mid 시간 기준 몇 명의 인원이 처리되었는지 확인
     if (count >= n) {
         minimumTime = mid;
         return BinarySearch(first, mid - 1, n, times);
