163139a4283153e31521ca43c10a00ff5c1b43c3
Jiyeon Jeong <jiyeon2781@naver.com>
2024-08-07 11:05:03 +0900

    2024.08.07-algorithm-practice
    backtracking practice

---
+++ Baekjoon/Silver/15654.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+void Backtracking(int depth, int M);
+
+vector<int> nums;
+vector<int> result;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int N, M, input;
+
+    cin >> N >> M;
+
+    while (N--) {
+        cin >> input;
+        nums.push_back(input);
+    }
+    sort(nums.begin(), nums.end());
+
+    Backtracking(0, M);
+}  
+
+void Backtracking(int depth, int M) {
+    if (depth == M) {
+        for (auto element : result) cout << element << ' ';
+        cout << "\n";
+        return;
+    }
+
+    for (int i = 0; i < nums.size(); i++) {
+        if (find(result.begin(), result.end(), nums[i]) != result.end()) continue;
+        result.push_back(nums[i]);
+        int idx = result.size() - 1;
+        Backtracking(depth + 1, M);
+        result.erase(result.begin() + idx);
+    }
+}
